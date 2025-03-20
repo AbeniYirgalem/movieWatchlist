cddbcd203e298ee8d0e416b36a63b906655202d0
Abeni <abenezeryirgalem0@gmail.com>
2025-03-20 21:23:20 +0300

    Update fetch logic to use process.env for API key access

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,13 +1,13 @@
 import React, { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { myFavorite } from "../context/FavoriteContext";
+const apiKey = import.meta.env.VITE_TMDB_API_KEY;
 
 const Movie = () => {
   const [movies, setMovies] = useState([]);
   const { favorite, setFavorite } = useContext(myFavorite);
 
-  const URL =
-    "https://api.themoviedb.org/3/movie/popular?api_key=ad99fe54e262f888c08925483eeeab9a";
+  const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${apiKey}`;
 
   useEffect(() => {
     const fetchData = async () => {
@@ -40,34 +40,38 @@ const Movie = () => {
             <p>Loading...</p>
           </div>
         ) : (
-          movies.map((movie) => ( movie.poster_path === '' ? '' :
-            <div className="col" key={movie.id}>
-              <div className="card h-100">
-                <img
-                  src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
-                  className="card-img-top"
-                  alt={movie.title}
-                />
-                <div className="card-body">
-                  <h5 className="card-title">{movie.title}</h5>
-                  <div className="d-grid gap-2">
-                    <button 
-                      className="btn btn-danger" 
-                      onClick={() => handleDelete(movie.id)}
-                    >
-                      Delete
-                    </button>
-                    <button 
-                      className="btn btn-primary" 
-                      onClick={() => handleAdd(movie.id)}
-                    >
-                      Add to Favorites
-                    </button>
+          movies.map((movie) =>
+            movie.poster_path === "" ? (
+              ""
+            ) : (
+              <div className="col" key={movie.id}>
+                <div className="card h-100">
+                  <img
+                    src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
+                    className="card-img-top"
+                    alt={movie.title}
+                  />
+                  <div className="card-body">
+                    <h5 className="card-title">{movie.title}</h5>
+                    <div className="d-grid gap-2">
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => handleDelete(movie.id)}
+                      >
+                        Delete
+                      </button>
+                      <button
+                        className="btn btn-primary"
+                        onClick={() => handleAdd(movie.id)}
+                      >
+                        Add to Favorites
+                      </button>
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-          ))
+            )
+          )
         )}
       </div>
     </div>
